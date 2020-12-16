bf9ea01bc781ca6df1c077467edb3d588dcc0c63
elu206 <elu206@nyu.edu>
2020-12-15 20:32:45 -0800

    minor changes & comments added

--- sketch.js
+++ sketch.js
@@ -20,21 +20,18 @@
 // Sprites are finished
 
 let state = 1; //tell which screen its on
-let clouds = [];
+let clouds = []; //cloud array
+let sky, goathead, myCloud, twogoats, goatlost, grassimg, waterimg, farm, bird; //images
+let myFont; //fonts
+let bgmusic, splash, squawk; //sounds
 let mic; //microphone
-let SCENE_W = 1300 * 10; //scene width - using camera for sidescrolling effect
-let ground; //group for ground
+let SCENE_W = 13000; //scene width - using camera for sidescrolling effect
+let ground, grass, birds; //groups
 let grounded = true; //boolean to check if landed after jumping
-let grass;
 let gravity = 5;
 let jump = -6;
-let title;
-let losewords;
-let winwords;
-let howtoplaywords;
-let xpos, ypos;
-let goat;
-let birds = []
+let title, losewords, winwords, howtoplaywords, xpos, ypos; //text
+let goat; //player character
 
 function preload(){ //loading images, font, sounds
   sky = loadImage('images/background sky.png');
@@ -48,7 +45,9 @@ function preload(){ //loading images, font, sounds
 	farm = loadImage('images/farm.png');
 	//carrot = loadImage('carrot1.png');
 	bird = loadImage('images/bird1.png');
+
 	myFont = loadFont('fonts/FredokaOne-Regular copy.ttf');
+
 	bgmusic = loadSound('sounds/background music.mp3');
 	bgmusic.setVolume(0.1);
 	splash = loadSound('sounds/splash.wav');
@@ -76,23 +75,19 @@ function setup() {
   // cloud1 = new Clouds(200, 100, 1.5); //creating clouds
   // cloud2 = new Clouds(1000, 250, 2);
   // cloud3 = new Clouds(750, 400, 1);
-
-	//grass
-	createCanvas(1300, 725);
-	bgmusic.loop();
 	mic = new p5.AudioIn(); //allowing for microphone input
 	mic.start();
 	ground = new Group();
 	water = new Group();
 	birds = new Group();
 	for (let x = 50; x < SCENE_W + width; x += 100) { //creating grass sprites for beginning
-		if (x < width){
+		if (x < width){ //positioning grass sprites
 			grass = createSprite(x, height - 50);
 			grass.addImage(grassimg);
 			ground.add(grass);
 		} else if (x > width && x < SCENE_W){
 				if ((x > width + 300 && x < width + 500) || (x > width + 900 && x < width + 1200) || (x > width + 1500 && x < width + 1700)|| (x > width + 2300 && x < width + 2500)|| (x > width + 3000 && x < width + 3500) || (x > width + 3700 && x < width + 4300)|| (x > width + 5000 && x < width + 5300)|| (x > width + 5700 && x < width + 5900)|| (x > width + 6000 && x < width + 6300) || (x > width + 6800 && x < width + 7300)|| (x > width + 7900 && x < width + 8300)|| (x > width + 9000 && x < width + 9500) || (x > width + 9700 && x < width + 9900) || (x > width + 10000 && x < width + 10300)){
-					thewater = createSprite(x, height - 50);
+					thewater = createSprite(x, height - 50); //positioning water
 					thewater.addImage(waterimg);
 					thewater.setCollider("rectangle", 50, 50, 100, 50); //fixing the collision detection for water
 					water.add(thewater)
@@ -113,17 +108,18 @@ function setup() {
 // 		}
 // 	}
 
-	for (let i = width + 800; i < SCENE_W; i += 1500){
+	for (let i = width + 800; i < SCENE_W; i += 1500){ //bird obstacles
 		itsabird = createSprite(i, random(600, 250))
 		itsabird.addImage(bird);
-		itsabird.setCollider("circle", 0, 0, 10)
+		itsabird.setCollider("circle", 0, 0, 10) //collision circle
 		birds.add(itsabird);
 	}
 
 	goat = createSprite(width/2, height-300); //creating goat sprite
-	let myAnimation = goat.addAnimation('running','images/goat_run1.png', 'images/idlegoat.png','images/goat_run2.png')
+
+	let myAnimation = goat.addAnimation('running','images/goat_run1.png', 'images/idlegoat.png','images/goat_run2.png') //running animation
 	myAnimation.offY = 18;
-	goat.addAnimation('jumping', 'images/goat_scream.png')
+	goat.addAnimation('jumping', 'images/goat_scream.png') //scream with the goat!
 }
 
 function draw(){
@@ -137,7 +133,7 @@ function draw(){
   // cloud2.move();
   // cloud3.move();
 
-	for (let i=0; i < 30; i++){
+	for (let i=0; i < 30; i++){ //display cloud array
 		clouds[i].move();
 		clouds[i].display();
   }
@@ -157,9 +153,9 @@ function draw(){
 	imageMode(CENTER);
 
 	//controlling which screen you are on through state variable
-  if (state == 1){ //home screen
+  if (state == 1){
     homeScreen();
-  } else if (state == 2){ //game state
+  } else if (state == 2){
     game();
   } else if (state == 3){
 		howToPlay();
@@ -200,7 +196,7 @@ function game(){ //code for game (STATE = 2)
 		goat.changeAnimation('running')
 	}
 
-	goat.collide(ground); //collision with ground
+	goat.collide(ground); //collision detection with ground
 
 	if (goat.collide(ground)){
 		grounded = true
@@ -240,7 +236,6 @@ function game(){ //code for game (STATE = 2)
 	}
 
 	image(farm, SCENE_W + width/2 - 300, height /2 + 13, 600, 500);
-	 //collision detection for goat & grass sprites
 
 	drawSprites();
 }
@@ -251,8 +246,7 @@ function howToPlay(){ //code for rules page (STATE = 3)
 		text(howtoplaywords[i], xpos, ypos + random(-2, 2));
 		xpos += textWidth(howtoplaywords[1])
 	}
-
-	xpos = 250
+	xpos = 250;
 
 	textSize(30);
 	strokeWeight(5);
@@ -274,7 +268,7 @@ function loseScreen(){ //code for lose (STATE = 4)
 		text(losewords[i], xpos, ypos + random(-2, 2));
 		xpos += textWidth(losewords[1])
 	}
-	xpos = 275
+	xpos = 275;
 
 	image(goatlost, width/2, 330, 450, 450); //goat lost
 	textSize(50);
@@ -289,9 +283,9 @@ function winScreen(){ //code for win (STATE = 5)
 	textSize(100);
   for (let i = 0; i < winwords.length; i ++){
 		text(winwords[i], xpos, ypos + random(-2, 2));
-		xpos += textWidth(winwords[1])
+		xpos += textWidth(winwords[1]);
 	}
-	xpos = 375
+	xpos = 375;
 
 	image(twogoats, width/2, 330, 650, 450); //goat is back with goat dad image
 	textSize(50);
@@ -302,7 +296,7 @@ function winScreen(){ //code for win (STATE = 5)
 	image(farm, width - 300, height /2 + 13, 600, 500);
 }
 
-function keyPressed(){
+function keyPressed(){ //control which screen you are on
   if (state == 1 || state == 4 || state == 5){
     if (keyCode == ENTER){ // press enter to start game
       state = 2;
